465bae0dbd7f8fdf73f5b8efc0ea401709d761a0
sdevaraj <sdevaraj@pandora.com>
2018-03-04 11:29:08 -0800

    Redirect to swagger from root dir

--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from rest_framework_swagger.views import get_swagger_view
 
@@ -23,6 +24,7 @@ from weather import urls as api_urls
 urls_documented = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urls.urlpatterns)),
+    url(r'^$', RedirectView.as_view(url='/api/schema/', permanent=False), name='index')
 ]
 
 schema_view = get_swagger_view(title='Weather API documentation', patterns=urls_documented)
