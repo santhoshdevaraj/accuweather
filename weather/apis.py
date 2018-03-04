39b735ad6d4871360e93791ee1da29e15f1aa625
sdevaraj <sdevaraj@pandora.com>
2018-03-04 01:21:32 -0800

    Code for calculating weekly averages and refactoring to move common code

--- weather/apis.py
+++ weather/apis.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.http import HttpResponseBadRequest
 
@@ -58,46 +58,64 @@ class WeatherDetailViewSet(viewsets.ModelViewSet):
     def get_queryset(self, *args, **kwargs):
         return models.WeatherDetail.objects.all()
 
-    def create_weekly_response(self, response):
-        pass
+    @staticmethod
+    def get_year_month_week(date_object):
+        """Given a date object returns the year, month and start of week"""
+        return (
+            date_object.year,
+            date_object.strftime('%B'),
+            (date_object - timedelta(days=date_object.weekday())).strftime('%Y-%m-%d')
+        )
 
-    def create_monthly_response(self, response):
-        """Updates the response with the year and month and average for that month"""
-        data_map = defaultdict(lambda : (0, 0, 0, 0))
-        data = response.data
-
+    @staticmethod
+    def get_data_map(data, data_map, frequency):
+        """Formats the data based on the input frequency passed"""
         for row in data:
             current_date, tmax, tmin = row['date'], row['tmax'], row['tmin']
-            date_object = datetime.strptime(current_date, '%Y-%m-%d')
-            year, month = date_object.year, date_object.strftime('%B')
-            tmax_total, tmin_total, max_days, min_days = data_map[(year, month)]
-            data_map[(year, month)] = (
+            year, month, week = WeatherDetailViewSet.get_year_month_week(datetime.strptime(current_date, '%Y-%m-%d'))
+            key = (year, month) if frequency == 'month' else (year, week)
+
+            tmax_total, tmin_total, max_days, min_days = data_map[key]
+            data_map[key] = (
                 tmax_total+tmax if tmax else tmax_total,
                 tmin_total+tmin if tmin else tmin_total,
                 max_days+1 if tmax else max_days,
                 min_days+1 if tmin else min_days
             )
+        return data_map
 
-        monthly_data = []
+    def get_response(self, data_map, frequency):
+        """Creates the response, based on the frequency requested"""
+        data = []
 
         for key in data_map:
-            total_tmax, total_tmin, max_days, min_days, year, month = *data_map[key], *key
-            monthly_data.append({
-                "year": year,
-                "month": month,
-                "avg_max": round(total_tmax/max_days) if max_days else "N/A",
-                "avg_min": round(total_tmin/min_days) if min_days else "N/A"
+            total_tmax, total_tmin, max_days, min_days, year, freq_value = *data_map[key], *key
+            data.append({
+                'year': year,
+                frequency: freq_value,
+                "avg_max": round(total_tmax / max_days) if max_days else "N/A",
+                "avg_min": round(total_tmin / min_days) if min_days else "N/A"
             })
 
-        return monthly_data
+        return data
+
+    def create_weekly_response(self, response, frequency):
+        """Updates the response with the year and week and average for that month"""
+        data_map = self.get_data_map(response.data, defaultdict(lambda: (0, 0, 0, 0)), frequency)
+        return self.get_response(data_map, frequency)
+
+    def create_monthly_response(self, response, frequency):
+        """Updates the response with the year and month and average for that month"""
+        data_map = self.get_data_map(response.data, defaultdict(lambda : (0, 0, 0, 0)), frequency)
+        return self.get_response(data_map, frequency)
 
     def update_for_frequency(self, response, request):
         """Based on the frequency passed updates the API response accordingly"""
         frequency = request.query_params.get('frequency', None)
         if frequency == 'weekly':
-            return self.create_weekly_response(response)
+            return self.create_weekly_response(response, 'week')
         elif frequency == 'monthly':
-            return self.create_monthly_response(response)
+            return self.create_monthly_response(response, 'month')
         return response.data
 
     def list(self, request, *args, **kwargs):
